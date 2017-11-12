9dd1367706602d408a5de6eb69d1cae075f71191
Thierry Chappuis <tchappui@osx.chappuis.net>
2017-11-12 13:56:13 +0100

    Files added to the project

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use App\Model\TestClass;
+
+$test = new TestClass();
+
+var_dump($test);
---
+++ src/Model/TestClass.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+class TestClass
+{
+    public function __construct()
+    {
+        echo('Welcome to test');
+    }
+}
